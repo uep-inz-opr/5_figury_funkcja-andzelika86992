0136116cbd8fae5eb8235687220261daa882ab07
andzelika86992 <102252481+andzelika86992@users.noreply.github.com>
2022-06-01 18:19:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import math
 
-def pole_kola(r):
-    return math.pi*(r*r)
-
-def pole_prostokata(a,b):
-    return (a*a)+(b*b)
-
-def pole_trojkata(x,y,z):
-    p= (x+y+z)/2
-    return math.sqrt(p*((p-x)*(p-y)*(p-z)))
-
 liczba= []
 liczba_figur= int(input())
 for i in range(liczba_figur):
     liczba.append(input().split(" "))
 
-for dane in liczba:
-    if len(dane) == 1:
-        kolo= pole_kola(float(dane[0]))
-    elif len(dane) == 2:
-        prostokat= pole_prostokata(float(dane[0]), float(dane[1]))
-    elif len(dane) == 3:
-        trojkat= pole_trojkata(float(dane[0]), float(dane[1]), float(dane[2]))
-    elif len(dane) > 3:
-        print("Błąd: można podać maksymalnie 3 liczby")
 
-    def suma_pol(prostokat, trojkat, kolo):
-        return prostokat+kolo+trojkat
+def pole_kola(r):
+    return math.pi*r*r
+
+def pole_prostokata(a,b):
+    return a*b
+
+def pole_trojkata(x,y,z):
+    p= (x+y+z)/2
+    return math.sqrt(p*((p-x)*(p-y)*(p-z)))
+
+pole= 0
+for dane in liczba:
+    if len(dane)== 1:
+        kolo= pole_kola(float(dane[0]))
+        pole += kolo
+    elif len(dane)== 2:
+        prostokat= pole_prostokata(float(dane[0]), float(dane[1]))
+        pole += prostokat
+    elif len(dane)== 3:
+        trojkat= pole_trojkata(float(dane[0]), float(dane[1]), float(dane[2]))
+        pole += trojkat
+    elif len(dane)> 3:
+        print("Błąd: można podać maksymalnie 3 liczby")
     
-    suma= suma_pol(prostokat, trojkat, kolo)
-    print(round(suma, 2))
+print(round(pole, 2))
+ 
